a68282a0acd0a125ee2aea5212a5c80ad8f386b6
Paul Brook <paul@nowt.org>
2019-12-06 21:54:16 +0000

    2019/6 #2

--- 2019/6/main.hs
+++ 2019/6/main.hs
@@ -18,13 +18,33 @@ buildMass orbits name = let
 numSat :: Mass -> Int
 numSat (Mass name s) = length s + sum (map numSat s)
 
+totalOrbits :: Mass -> Int
 totalOrbits m@(Mass name s) = numSat m + sum (map totalOrbits s)
 
-part1 input = let
-        com = buildMass input "COM"
-    in totalOrbits com
+part1 = totalOrbits
+
+findMass :: String -> Mass -> [String]
+findMass dest (Mass name s)
+    | name == dest = [name]
+    | otherwise = let found = concat (map (findMass dest) s)
+        in case found of
+            [] -> []
+            _ -> name:found
+
+uniqueOrbits :: [String] -> [String] -> Int
+uniqueOrbits (x:xs) (y:ys)
+    | x == y = uniqueOrbits xs ys
+    | otherwise = length xs + length ys
+
+part2 com = let
+        you = findMass "YOU" com
+        san = findMass "SAN" com
+    in uniqueOrbits you san
 
 main = do
     raw <- readFile "input"
-    let input = parse raw
-    print $ part1 input
+    let
+        input = parse raw
+        com = buildMass input "COM"
+    print $ part1 com
+    print $ part2 com
