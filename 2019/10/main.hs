b5fc02ae068e57e6ea84e36f86678ff7487c904c
Paul Brook <paul@nowt.org>
2019-12-11 00:54:38 +0000

    2019/10 #2

--- 2019/10/main.hs
+++ 2019/10/main.hs
@@ -4,7 +4,7 @@ import Data.List
 import Data.Ratio
 
 data Point = Point Int Int deriving Show
-data Direction = Direction Int Int deriving (Show, Eq)
+data Vector = Vector Int Int Int deriving Show
 
 parseLine :: (Int, Int) -> String -> [Maybe Point]
 parseLine _ [] = []
@@ -21,22 +21,79 @@ parse x = let
         (_, p) = mapAccumL parseGrid 0 (lines x)
     in concat p
 
-getDirection :: Point -> Point -> Direction
-getDirection (Point ax ay) (Point bx by) = let
-        x = ax - bx
-        y = ay - by
+getVector :: Point -> Point -> Maybe Vector
+getVector (Point ax ay) (Point bx by) = let
+        x = bx - ax
+        y = by - ay
     in if x == 0 && y == 0
-        then Direction 0 0
-        else let d = gcd x y in Direction (x `div` d) (y `div` d)
+        then Nothing
+        else let d = gcd x y in Just (Vector (x `div` d) (y `div` d) d)
 
-countVisible :: [Point] -> Point -> Int
-countVisible others me = let
-        vectors = map (getDirection me) others
-    in length (nub vectors)
+-- We don't actually need an accurate angle, a (cheaper) slope value would be
+-- sufficient.  However atan2 already knows how to map quadrants correctly,
+-- so for simplicitly we use that
+-- (atan2 y x) gives the angle from the x-axis.
+-- We want clockwise from the y-axis, so (atan2 -x y)
+angle :: Vector -> Float
+angle (Vector x y _)
+    -- Special case the ambiguous boundary case to make sure it is at the start
+    | x == 0 && y < 0 = -pi
+    | otherwise = atan2 (-fromIntegral x) (fromIntegral y)
 
-part1 a = maximum (map (countVisible a) a) - 1
+compareVector :: Vector -> Vector -> Ordering
+compareVector a@(Vector _ _ ad) b@(Vector _ _ bd)
+    | sameDirection a b = ad `compare` bd
+    | otherwise = (angle a) `compare` (angle b)
+
+sameDirection :: Vector -> Vector -> Bool
+sameDirection (Vector ax ay _) (Vector bx by _) = (ax == bx) && (ay == by)
+
+getVectors :: [Point] -> Point -> (Int, [Vector], Point)
+getVectors others me = let
+        v = mapMaybe (getVector me) others
+        v' = sortBy compareVector v
+        (a, b) = sweep v'
+    in (length a, a ++ sweepAll b, me)
+
+bestVector :: (Int, [Vector], Point) -> (Int, [Vector], Point) -> (Int, [Vector], Point)
+bestVector (an, av, ap) (bn, bv, bp)
+    | an > bn = (an, av, ap)
+    | otherwise = (bn, bv, bp)
+
+bestStation :: [Point] -> (Int, [Vector], Point)
+bestStation a = let
+        v = map (getVectors a) a
+    in foldl1 bestVector v
+
+
+sweep :: [Vector] -> ([Vector], [Vector])
+sweep [] = ([], [])
+sweep [x] = ([x], [])
+sweep (x:y:ys)
+    | sameDirection x y = let
+            (a, b) = sweep (x:ys)
+        in (a, y:b)
+    | otherwise = let
+            (a, b) = sweep (y:ys)
+        in (x:a, b)
+
+sweepAll :: [Vector] -> [Vector]
+sweepAll x = let
+        (a, b) = sweep x
+    in a ++ case b of
+        [] -> []
+        _ -> sweepAll b
+
+part2 v (Point px py) = let
+        v' = sweepAll v
+        (Vector x y d) = v' !! 199
+        x' = x * d + px
+        y' = y * d + py
+    in x' * 100 + y'
 
 main = do
     raw <- readFile "input"
     let input = parse raw
-    print $ part1 input
+        (n, v, p) = bestStation input
+    print $ n
+    print $ part2 v p
