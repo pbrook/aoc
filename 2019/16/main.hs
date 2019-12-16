9aeafd8a5be9d8bea307283db50d3c3cb28d4bdf
Paul Brook <paul@nowt.org>
2019-12-16 23:29:33 +0000

    2019/16 #2

--- 2019/16/main.hs
+++ 2019/16/main.hs
@@ -20,14 +20,29 @@ fftDigit n = (`mod` 10) . abs . sum . zipWith (*) (pattern n)
 fft :: [Int] -> [Int]
 fft x = [fftDigit n x | n <- [1..(length x)]]
 
-fftN :: Int -> [Int] -> [Int]
-fftN 0 x = x
-fftN n x = fftN (n-1) (fft x)
+applyN :: (a -> a) -> Int -> a -> a
+applyN f 0 x = x
+applyN f n x = applyN f (n-1) (f x)
 
-part1 inp = fftN 100 inp
+part1 inp = applyN fft 100 inp
+
+fftR :: [Int] -> [Int]
+fftR (x:xs) = map (`mod` 10) (scanl' (+) x xs)
+
+-- Notice that the last half of the fft is the sum of the subsequent elements
+part2 inp = let
+        len = 10000 * length inp
+        n = read (take 7 (map intToDigit inp)) :: Int
+    in case n > (len `div` 2) of
+        False -> undefined
+        True -> let
+                ri = reverse inp
+                t = take (len - n) (cycle ri)
+            in reverse (applyN fftR 100 t)
 
 main = do
     raw <- readFile "input"
     let
         inp = parse raw
     putStrLn $ top8 (part1 inp)
+    putStrLn $ top8 (part2 inp)
