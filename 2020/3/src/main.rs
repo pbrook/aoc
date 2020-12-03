4373dc4534ff8d41dbef85b97b26c38514edfe6d
Paul Brook <paul@nowt.org>
2020-12-03 21:22:11 +0000

    2020/3 #2

--- 2020/3/src/main.rs
+++ 2020/3/src/main.rs
@@ -11,14 +11,20 @@ fn parse(filename: &str) -> Vec<Vec<bool>> {
     return lines.map(|v| parse_line(v.unwrap())).collect();
 }
 
-fn part1(v: &Vec<Vec<bool>>) -> u32 {
+fn part1(v: &Vec<Vec<bool>>, dx: usize, dy: usize) -> u32 {
     let mut x = 0;
     let mut trees = 0;
+    let mut y = 0;
     for row in v {
+        if y > 0 {
+            y -= 1;
+            continue;
+        }
+        y = dy - 1;
         if row[x] {
             trees += 1;
         }
-        x += 3;
+        x += dx;
         if x >= row.len() {
             x -= row.len();
         }
@@ -26,7 +32,16 @@ fn part1(v: &Vec<Vec<bool>>) -> u32 {
     return trees;
 }
 
+fn part2(v: &Vec<Vec<bool>>) -> u32 {
+    let slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)];
+    let mut tot = 1;
+    for (x, y) in slopes.iter() {
+        tot *= part1(v, *x, *y);
+    }
+    return tot;
+}
 fn main() {
     let v = parse("input");
-    println!("{}", part1(&v))
+    println!("{}", part1(&v, 3, 1));
+    println!("{}", part2(&v));
 }
