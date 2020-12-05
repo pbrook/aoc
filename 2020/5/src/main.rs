8fc45e8e61cff925e1613cc6e87ad8919f232a89
Paul Brook <paul@nowt.org>
2020-12-05 12:31:33 +0000

    2020/5 #2

--- 2020/5/src/main.rs
+++ 2020/5/src/main.rs
@@ -13,15 +13,34 @@ fn part_index(s: &str) -> u32 {
     return base;
 }
 
-fn part1(filename: &str) -> u32 {
+fn parse(filename: &str) -> Vec<u32> {
     let file = File::open(filename).unwrap();
     let lines = io::BufReader::new(file).lines();
-    return lines.map(|v| part_index(v.unwrap().as_str())).max().unwrap();
+    return lines.map(|v| part_index(v.unwrap().as_str())).collect();
+}
+
+fn part1(v: &Vec<u32>) -> u32 {
+    return v[v.len() - 1];
+}
+
+fn part2(v: &Vec<u32>) -> u32 {
+    let invalid = 1 << 10;
+    let mut prev = invalid;
+    for &i in v.iter() {
+        if prev == i - 2 {
+            return i - 1;
+        }
+        prev = i;
+    }
+    return invalid;
 }
 
 fn main() {
     assert_eq!(part_index(&"BFFFBBFRRR"), 567);
     assert_eq!(part_index(&"FFFBBBFRRR"), 119);
     assert_eq!(part_index(&"BBFFBBFRLL"), 820);
-    println!("{}", part1("input"));
+    let mut v = parse("input");
+    v.sort();
+    println!("{}", part1(&v));
+    println!("{}", part2(&v));
 }
