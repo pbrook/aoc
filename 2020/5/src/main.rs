40032c8a797f754c1bf0d0b56adbfb1df78b08ed
Paul Brook <paul@nowt.org>
2020-12-05 14:06:19 +0000

    2020/5 better algorithm

--- 2020/5/src/main.rs
+++ 2020/5/src/main.rs
@@ -13,34 +13,52 @@ fn part_index(s: &str) -> u32 {
     return base;
 }
 
-fn parse(filename: &str) -> Vec<u32> {
+fn parse(filename: &str) {
     let file = File::open(filename).unwrap();
     let lines = io::BufReader::new(file).lines();
-    return lines.map(|v| part_index(v.unwrap().as_str())).collect();
-}
 
-fn part1(v: &Vec<u32>) -> u32 {
-    return v[v.len() - 1];
-}
+    let mut min = 1 << 10;
+    let mut max = 0;
+    let mut val = 0;
+    let mut n = 0;
 
-fn part2(v: &Vec<u32>) -> u32 {
-    let invalid = 1 << 10;
-    let mut prev = invalid;
-    for &i in v.iter() {
-        if prev == i - 2 {
-            return i - 1;
+    for seat in lines.map(|v| part_index(v.unwrap().as_str())) {
+        if seat < min {
+            min = seat;
         }
-        prev = i;
+        if seat > max {
+            max = seat;
+        }
+        n += 1;
+        // xor all the seat IDs together. Each block of 4 seats (half a row)
+        // will cancel each other out. The problem states that the only 
+        // missing seats are at the front and back. If we pad those to a
+        // multiple of 4, the result will be the ID that is missing.
+        // Credit to jbert for the idea
+        val ^= seat;
     }
-    return invalid;
+
+    // Check all seats ids were contiguous (except the missing one!)
+    assert_eq!(n, max - min);
+    // Populate the missing seatch in the first/last [half] row
+    let mut extra = max + 1;
+    while (extra & 3) != 0 {
+        val ^= extra;
+        extra += 1;
+    }
+    let mut extra = min;
+    while (extra & 3) != 0 {
+        extra -= 1;
+        val ^= extra;
+    }
+    println!("{}", max);
+    println!("{}", val);
 }
+
 
 fn main() {
     assert_eq!(part_index(&"BFFFBBFRRR"), 567);
     assert_eq!(part_index(&"FFFBBBFRRR"), 119);
     assert_eq!(part_index(&"BBFFBBFRLL"), 820);
-    let mut v = parse("input");
-    v.sort();
-    println!("{}", part1(&v));
-    println!("{}", part2(&v));
+    parse("input");
 }
