09ee754a8746cd1ebc864267380f62df44405bd0
Paul Brook <paul@nowt.org>
2020-12-08 22:42:49 +0000

    2020/7 Do hash loopups during parsing

--- 2020/7/src/main.rs
+++ 2020/7/src/main.rs
@@ -1,104 +1,103 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::collections::HashMap;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 
 use regex::Regex;
 
+type BagRef = Rc<RefCell<Bag>>;
+type BagHash = HashMap<String, BagRef>;
+
 struct BagCount {
-    name: String,
+    bag: BagRef,
     count: u32,
 }
 
+#[derive(Default)]
 struct Bag {
-    name: String,
-    holds: Vec<BagCount>
+    shiny: bool,
+    size: Option<u32>,
+    holds: Vec<BagCount>,
 }
 
-fn parse_line(s: String) -> Bag {
+fn find_bag(name: String, h: &mut BagHash) -> BagRef {
+    h.entry(name).or_default().clone()
+}
+
+fn parse_line(s: String, h: &mut BagHash) -> BagRef {
     lazy_static! {
         static ref NAME_RE: Regex = Regex::new(r"(.*) bags contain").unwrap();
         static ref HOLDS_RE: Regex = Regex::new(r"(\d+) ([^,]*) bag").unwrap();
     }
     let name = NAME_RE.captures(&s).unwrap()[1].to_string();
-    let holds = HOLDS_RE.captures_iter(&s).map(
-        |c| BagCount{count:c[1].parse::<u32>().unwrap(), name: c[2].to_string()}
+    let bag = find_bag(name, h);
+    bag.borrow_mut().holds =
+        HOLDS_RE.captures_iter(&s).map(
+        |c| BagCount{count:c[1].parse::<u32>().unwrap(), bag: find_bag(c[2].to_string(), h)}
         ).collect();
-    return Bag{name: name, holds: holds};
+    return bag;
 }
 
-fn parse(filename: &str) -> Vec<Bag> {
+fn parse(filename: &str) -> (u32, u32) {
     let file = File::open(filename).unwrap();
     let lines = io::BufReader::new(file).lines();
-    return lines.map(|v| parse_line(v.unwrap())).collect();
-}
+    let mut h = HashMap::new();
+    for v in lines {
+        parse_line(v.unwrap(), &mut h);
+    }
 
-
-fn part1(v: &Vec<Bag>) -> usize {
-    let mut shiny: HashMap<String, ()> = HashMap::new();
-    shiny.insert("shiny gold".to_string(), ());
+    let gold = &h["shiny gold"];
+    // could release hashmap here, we just need to be able to interate over
+    // all the bags
+    let mut scount = 0;
     let mut again = true;
+    gold.borrow_mut().shiny = true;
+
     while again {
         again = false;
-        for b in v {
-            let outer = &b.name;
-            if shiny.contains_key(outer) {
-                continue;
-            }
-            for held in &b.holds {
-                if shiny.contains_key(&held.name) {
-                    shiny.insert(outer.clone(), ());
+        for outer_ref in h.values() {
+            let mut outer = outer_ref.borrow_mut();
+            if !outer.shiny {
+                if outer.holds.iter().any(|inner| inner.bag.borrow().shiny) {
+                    outer.shiny = true;
                     again = true;
-                    break;
+                    scount += 1;
                 }
+            }
+            if outer.size.is_none() {
+                let mut size: Option<u32> = Some(0);
+
+                for held in &outer.holds {
+                    let inner = held.bag.borrow();
+                    if inner.size.is_none() {
+                        size = None;
+                        break;
+                    }
+                    size = size.map(|n| n + (inner.size.unwrap() + 1) * held.count);
+                }
+                outer.size = size;
             }
         }
-    }
-    return shiny.len() - 1;
-}
-
-fn part2(v: &Vec<Bag>) -> u32 {
-    let mut sm: HashMap<String, u32> = HashMap::new();
-    loop {
-        for b in v {
-            let outer = &b.name;
-            if sm.contains_key(outer) {
-                continue;
-            }
-            let mut size: Option<u32> = Some(0);
-
-            for held in &b.holds {
-                let inner = sm.get(&held.name);
-                if inner.is_none() {
-                    size = None;
-                    break;
-                }
-                size = size.map(|n| n + (inner.unwrap() + 1) * held.count);
-            }
-            if size.is_some() {
-                let n = size.unwrap();
-                if outer == "shiny gold" {
-                    return n;
-                }
-                sm.insert(outer.clone(), n);
-            }
+        if gold.borrow().size.is_none() {
+            again = true;
         }
     }
+    return (scount, gold.borrow().size.unwrap());
 }
+
 
 fn test() {
-    let v = parse("test");
-    assert_eq!(part1(&v), 4);
-    assert_eq!(part2(&v), 32);
-    let v = parse("test2");
-    assert_eq!(part2(&v), 126);
+    assert_eq!(parse("test"), (4, 32));
+    assert_eq!(parse("test2"), (0, 126));
 }
 
 fn main() {
     test();
-    let v = parse("input");
-    println!("{}", part1(&v));
-    println!("{}", part2(&v));
+    let (p1, p2) = parse("input");
+    println!("{}", p1);
+    println!("{}", p2);
 }
