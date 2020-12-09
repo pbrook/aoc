5ee8e6fbd67137c53748fc9e9d9c40df7c35d25d
Paul Brook <paul@nowt.org>
2020-12-09 18:30:14 +0000

    2020/9 #2

--- 2020/9/src/main.rs
+++ 2020/9/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::cmp::Ordering;
 
 fn parse(filename: &str) -> Vec<u64> {
     let file = File::open(filename).unwrap();
@@ -14,15 +15,10 @@ fn find_sum(v: &Vec<u64>, target: u64) -> bool {
 
     loop {
         let tot = v[a] + v[b];
-        if tot == target {
-            return true;
-        }
-        // If total is not big enough then pick the next bigger "small" value
-        // If totol too big then pick the next smaller "big" value.
-        if tot < target {
-            a += 1;
-        } else {
-            b -= 1;
+        match tot.cmp(&target) {
+            Ordering::Equal => return true,
+            Ordering::Less => a += 1,
+            Ordering::Greater => b -= 1,
         }
         if a >= b {
             return false;
@@ -30,7 +26,7 @@ fn find_sum(v: &Vec<u64>, target: u64) -> bool {
     }
 }
 
-fn part1(preamble: usize, input: &Vec<u64>) -> u64 {
+fn part1(input: &Vec<u64>, preamble: usize) -> u64 {
     let mut v = input[..preamble].to_vec();
     let mut offset = 0;
 
@@ -50,14 +46,40 @@ fn part1(preamble: usize, input: &Vec<u64>) -> u64 {
     panic!("no result found");
 }
 
+fn part2(v: &Vec<u64>, target: u64) -> u64 {
+    let mut start = 0;
+    let mut end = 0;
+    let mut sum = 0;
+    loop {
+        match sum.cmp(&target) {
+            Ordering::Equal => {
+                let range = &v[start..end];
+                return range.iter().min().unwrap() + range.iter().max().unwrap();
+            }
+            Ordering::Less => {
+                sum += v[end];
+                end += 1;
+            },
+            Ordering::Greater => {
+                sum -= v[start];
+                start += 1;
+            },
+        }
+    }
+}
+
 fn test() {
     let v = parse("test");
-    assert_eq!(part1(5, &v), 127);
+    let p1 = part1(&v, 5);
+    assert_eq!(p1, 127);
+    assert_eq!(part2(&v, p1), 62);
 }
 
 fn main() {
     test();
 
     let v = parse("input");
-    println!("{}", part1(25, &v));
+    let p1 = part1(&v, 25);
+    println!("{}", p1);
+    println!("{}", part2(&v, p1));
 }
