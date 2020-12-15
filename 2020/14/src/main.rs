9cb1d94620687edd5e30e7a1cff79d3f12a2445f
Paul Brook <paul@nowt.org>
2020-12-15 00:53:31 +0000

    2020/14 #2

--- 2020/14/src/main.rs
+++ 2020/14/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+#[derive(Debug,Copy,Clone)]
 enum Insn {
     Mask(u64, u64), // ones, zeros
     Write(usize, u64), // addr, val
@@ -28,7 +29,7 @@ fn parse_line(s: String) -> Insn {
         return parse_mask(&s[7..]);
     }
     // mem [...] =
-    let addr: usize = s[4..eq-2].parse().unwrap();
+    let addr = s[4..eq-2].parse().unwrap();
     let val: u64 = s[eq+2..].parse().unwrap();
     return Insn::Write(addr, val);
 }
@@ -41,8 +42,8 @@ fn parse(filename: &str) -> Vec<Insn> {
 
 fn part1(prog: &Vec<Insn>) -> u64 {
     let mut memsz = 0;
-    for i in prog.iter() {
-        match *i {
+    for &i in prog.iter() {
+        match i {
             Insn::Write(addr,_) => memsz = memsz.max(addr),
             _ => (),
         }
@@ -59,13 +60,68 @@ fn part1(prog: &Vec<Insn>) -> u64 {
     return mem.iter().sum();
 }
 
+#[derive(Debug,Copy,Clone)]
+struct Written {
+    mask: u64,
+    addr: u64,
+    val: u64,
+}
+
+// This assumes we have a small number of floating bits in each write
+// It will not work for the part1 example data, but works for the part2
+// example, and cofortably handles my input (max 9 floating bits per mask)
+fn try_sum(w: &Written, rest: &[Written]) -> u64 {
+    let mut count = 0;
+    let float = w.mask ^ ((1 << 36) - 1);
+    let mut bits = 0;
+    // Enumerate all the addresses, and test whether they are
+    // overwritten by a subsequent write
+    loop {
+        let addr = bits | w.addr;
+        if !rest.iter().any(|r| addr & r.mask == r.addr) {
+            count += 1;
+        }
+        bits = (bits + 1 + w.mask) & float;
+        if bits == 0 {
+            break;
+        }
+    }
+    return count * w.val;
+}
+
+fn part2(prog: &Vec<Insn>) -> u64 {
+    let mut writes = Vec::new();
+    let mut m0 = 0;
+    let mut m1 = 0;
+    // For convenience, explicit propagate the mask into each write
+    for &i in prog {
+        match i {
+            Insn::Mask(ones, zeros) => {m1 = ones; m0 = zeros},
+            Insn::Write(addr, val) => {
+                let mask = m1 | m0;
+                let maddr = (addr as u64 | m1) & mask;
+                writes.push(Written{mask:mask, addr:maddr, val:val});
+            }
+        }
+    }
+    // Now sum all the written values
+    let mut sum = 0;
+    for (n, w) in writes.iter().enumerate() {
+        sum += try_sum(w, &writes[n+1..]);
+    }
+    return sum;
+}
+
 fn test() {
     let v = parse("test");
     assert_eq!(part1(&v), 165);
+    let v = parse("test2");
+    assert_eq!(part2(&v), 208);
 }
 
 fn main() {
     test();
     let v = parse("input");
     println!("{}", part1(&v));
+    println!("{}", part2(&v));
 }
