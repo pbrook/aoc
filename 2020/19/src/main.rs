0c0fe94fe187ab1c66c01d0f12758d212d3e8081
Paul Brook <paul@nowt.org>
2020-12-20 10:06:22 +0000

    2020/19 #2

--- 2020/19/src/main.rs
+++ 2020/19/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,PartialEq)]
 enum Rule {
     Char(char),
     Chain(Vec<Vec<usize>>),
@@ -53,7 +53,6 @@ fn parse(filename: &str) -> Input {
 }
 
 fn match_rule(rules: &Vec<Rule>, n: usize, s: &str) -> Option<usize> {
-    //println!("rule   {} {}", n, s);
     let result = match &rules[n] {
         Rule::Char(c) => {
             if s.chars().next() == Some(*c) {Some(1)} else {None}
@@ -62,7 +61,6 @@ fn match_rule(rules: &Vec<Rule>, n: usize, s: &str) -> Option<usize> {
             let mut count = 0;
             let mut best = None;
             for opt in v {
-                //println!("opt    {} {:?}", n, opt);
                 let mut len = 0;
                 let mut ok = true;
                 for &sub in opt {
@@ -80,12 +78,10 @@ fn match_rule(rules: &Vec<Rule>, n: usize, s: &str) -> Option<usize> {
             best
         }
     };
-    //println!("Result {} {:?}", n, result);
     return result;
 }
 
-fn match_all(rules: &Vec<Rule>, s: &String) -> bool {
-    //println!("match_all {}", s);
+fn match_message(rules: &Vec<Rule>, s: &String) -> bool {
     match match_rule(rules, 0, s.as_str()) {
         Some(n) => n == s.len(),
         None => false,
@@ -93,16 +89,54 @@ fn match_all(rules: &Vec<Rule>, s: &String) -> bool {
 }
 
 fn part1(input: &Input) -> usize {
-    input.messages.iter().filter(|msg| match_all(&input.rules, msg)).count()
+    input.messages.iter().filter(|msg| match_message(&input.rules, msg)).count()
+}
+
+fn match_part2(rules: &Vec<Rule>, msg: &String) -> bool {
+    let mut n42 = 0;
+    let mut s = msg.as_str();
+    loop {
+        match match_rule(rules, 42, s) {
+            Some(n) => {
+                assert_eq!(match_rule(rules, 31, s), None);
+                n42 += 1;
+                s = &s[n..];
+            }
+            None => break,
+        }
+    }
+    let mut n31 = 0;
+    loop {
+        match match_rule(rules, 31, s) {
+            Some(n) => {
+                n31 += 1;
+                s = &s[n..];
+            }
+            None => break,
+        }
+    }
+    if s != "" {
+        return false;
+    }
+    return n42 > n31 && n31 > 0;
+}
+
+fn part2(input: &Input) -> usize {
+    assert!(input.rules[0] == Rule::Chain(vec![vec![8, 11]]));
+    input.messages.iter().filter(|msg| match_part2(&input.rules, msg)).count()
 }
 
 fn test() {
     let v = parse("test");
     assert_eq!(part1(&v), 2);
+    let v = parse("test2");
+    assert_eq!(part1(&v), 3);
+    assert_eq!(part2(&v), 12);
 }
 
 fn main() {
     test();
     let v = parse("input");
     println!("{}", part1(&v));
+    println!("{}", part2(&v));
 }
