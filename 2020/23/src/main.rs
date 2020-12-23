8b5779488fb5698e52dc150f0b10900dc5a6a307
Paul Brook <paul@nowt.org>
2020-12-23 13:29:35 +0000

    2020/23 Use array indexing rather than pointer chasing

--- 2020/23/src/main.rs
+++ 2020/23/src/main.rs
@@ -1,5 +1,4 @@
 use std::char;
-use std::cell::RefCell;
 
 fn parse(s: &str) -> Vec<u8> {
     return s.chars().map(|c| c.to_digit(10).unwrap() as u8).collect();
@@ -47,19 +46,12 @@ fn part1(input: &str) -> String {
     return v[1..].iter().map(|&n| char::from_digit(n as u32, 10).unwrap()).collect();
 }
 
-type ShellPtr<'a> = Option<&'a RefCell<Shell<'a>>>;
-
-struct Shell<'a> {
-    val: usize,
-    next: ShellPtr<'a>,
-    prev: ShellPtr<'a>,
+struct Shell {
+    next: usize,
+    prev: usize,
 }
 
 const NUM_SHELLS: usize = 1000000;
-
-impl Shell<'_> {
-    fn new<'a>(val: usize) -> Shell<'a> {Shell{val: val, next:None, prev:None}}
-}
 
 fn dec_p2(n: usize) -> usize {
     if n == 0 {
@@ -70,49 +62,50 @@ fn dec_p2(n: usize) -> usize {
 
 fn part2(input: &str) -> u64 {
     let mut shells = Vec::with_capacity(NUM_SHELLS);
-    for n in 0..NUM_SHELLS {
-        shells.push(RefCell::new(Shell::new(n)));
+    let input_len = input.len();
+    for _ in 0..input_len {
+        shells.push(Shell{next: 0, prev: 0});
     }
     let mut init = input.chars().map(|c| c.to_digit(10).unwrap() as usize - 1);
-    let n0 = init.next().unwrap();
-    let mut cur = &shells[n0];
+    let mut cur = init.next().unwrap();
     let mut prev = cur;
-    for n in init.chain(input.len()..NUM_SHELLS) {
-        let next = &shells[n];
-        next.borrow_mut().prev = Some(prev);
-        prev.borrow_mut().next = Some(next);
-        prev = next;
+    for n in init {
+        shells[prev].next = n;
+        shells[n].prev = prev;
+        prev = n;
     }
-    prev.borrow_mut().next = Some(cur);
-    cur.borrow_mut().prev = Some(prev);
+    shells[prev].next = input_len;
+    for n in input_len..NUM_SHELLS {
+        shells.push(Shell{next: n + 1, prev: n - 1});
+    }
+    shells[input_len].prev = prev;
+    shells[cur].prev = NUM_SHELLS - 1;
+    shells[NUM_SHELLS - 1].next = cur;
 
     for _ in 0..10000000 {
-        let n1 = cur.borrow().next;
-        let mut n1b = n1.unwrap().borrow_mut();
-        let n2 = n1b.next;
-        let n2b = n2.unwrap().borrow();
-        let n3 = n2b.next;
-        let mut n3b = n3.unwrap().borrow_mut();
-        let n4 = n3b.next;
-        n4.unwrap().borrow_mut().prev = Some(cur);
-        cur.borrow_mut().next = n4;
+        let n1 = shells[cur].next;
+        let n2 = shells[n1].next;
+        let n3 = shells[n2].next;
+        let n4 = shells[n3].next;
+        shells[n4].prev = cur;
+        shells[cur].next = n4;
 
-        let mut val = dec_p2(cur.borrow().val);
-        while n1b.val == val || n2b.val == val || n3b.val == val {
-            val = dec_p2(val);
+        let mut ins = dec_p2(cur);
+        while n1 == ins || n2 == ins || n3 == ins {
+            ins = dec_p2(ins);
         }
 
-        let ins = &shells[val];
-        n3b.next = ins.borrow().next;
-        n3b.next.unwrap().borrow_mut().prev = n3;
-        ins.borrow_mut().next = n1;
-        n1b.prev = Some(ins);
-        cur = n4.unwrap();
+        let next = shells[ins].next;
+        shells[n3].next = next;
+        shells[next].prev = n3;
+        shells[ins].next = n1;
+        shells[n1].prev = ins;
+
+        cur = n4;
     }
-    let n1 = shells[0].borrow();
-    let n2 = n1.next.unwrap().borrow();
-    let n3 = n2.next.unwrap().borrow();
-    return (n2.val + 1) as u64 * (n3.val + 1) as u64;
+    let s1 = shells[0].next;
+    let s2 = shells[s1].next;
+    return (s1 + 1) as u64 * (s2 + 1) as u64;
 }
 
 fn test() {
