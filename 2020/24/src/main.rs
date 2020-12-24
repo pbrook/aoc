a3e0f2f7a1080c03548d0d7e06786ec9c3f1781e
Paul Brook <paul@nowt.org>
2020-12-24 12:38:31 +0000

    2020/24 #1

---
+++ 2020/24/src/main.rs
@@ -0,0 +1,98 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::collections::HashSet;
+
+#[derive(Debug,PartialEq,Eq,Hash)]
+struct Point {
+    a: i32,
+    b: i32,
+}
+
+type Chain = Vec<Dir>;
+#[derive(Debug)]
+enum Dir {
+    E,
+    W,
+    SE,
+    SW,
+    NW,
+    NE
+}
+
+fn parse_line(s:String) -> Chain {
+    let mut result = Vec::new();
+    let mut c = s.chars();
+    loop {
+        match c.next() {
+            None => {return result;}
+            Some(ch) => {
+                result.push(match ch {
+                    'e' => Dir::E,
+                    'w' => Dir::W,
+                    'n' => match c.next().unwrap() {
+                        'e' => Dir::NE,
+                        'w' => Dir::NW,
+                        _ => panic!(),
+                    }
+                    's' => match c.next().unwrap() {
+                        'e' => Dir::SE,
+                        'w' => Dir::SW,
+                        _ => panic!(),
+                    }
+                    _ => panic!(),
+                });
+            }
+        }
+    }
+}
+
+fn parse(filename: &str) -> Vec<Chain> {
+    let file = File::open(filename).unwrap();
+    let lines = io::BufReader::new(file).lines();
+    return lines.map(|v| parse_line(v.unwrap())).collect();
+}
+
+fn follow(c: &Chain) -> Point {
+    // Choose NE (a) and NW (b) as our primary axes
+    let mut a = 0;
+    let mut b = 0;
+    for d in c {
+        match d {
+            Dir::NE => {a += 1}
+            Dir::SW => {a -= 1}
+            Dir::NW => {b += 1}
+            Dir::SE => {b -= 1}
+            Dir::E => {a += 1; b -= 1}
+            Dir::W => {a -= 1; b += 1}
+        }
+    }
+    return Point{a, b};
+}
+
+fn part1(input: &Vec<Chain>) -> usize {
+    let mut points = HashSet::new();
+    let mut count = 0;
+    for c in input {
+        let p = follow(c);
+        if points.contains(&p) {
+            points.remove(&p);
+            count -= 1;
+        } else {
+            points.insert(p);
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn test() {
+    let v = parse("test");
+    assert_eq!(follow(&parse_line("nwwswee".to_string())), Point{a: 0, b: 0});
+    assert_eq!(part1(&v), 10);
+}
+
+fn main() {
+    test();
+    let v = parse("input");
+    println!("{}", part1(&v));
+}
