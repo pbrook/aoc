506abb50cb1880fc845f8b0e9ae11a031b86e376
Paul Brook <paul@nowt.org>
2021-12-02 13:51:19 +0000

    2020/1 Use array intrinsics

--- 2021/1/main.f90
+++ 2021/1/main.f90
@@ -9,23 +9,16 @@ program one
     call assert(count_inc(a, 3), 5)
 
     call read_ints(a, 'input')
-    print *, count_inc(a, 1)
-    print *, count_inc(a, 3)
+    print *, 'Part1:', count_inc(a, 1)
+    print *, 'Part2:', count_inc(a, 3)
 contains
 
 function count_inc(a, sz) result(res)
     integer :: a(:)
     integer :: sz
     integer :: res
-    integer :: i
 
-    res = 0
-
-    do i = sz + 1, size(a)
-        if (a(i - sz) < a(i)) then
-            res = res + 1
-        end if
-    end do
+    res = count(a(1+sz:) > a(:size(a)-sz))
 end function
 
 end program
