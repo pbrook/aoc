ada0d3eb4ef9f0903be4bfea4216da425213ac54
Paul Brook <paul@nowt.org>
2021-12-03 16:42:07 +0000

    2021/3 #2

--- 2021/3/main.f90
+++ 2021/3/main.f90
@@ -2,39 +2,60 @@ program two
     use aoc
 
     implicit none
+    integer :: a(2)
 
-    call assert(part1('test'), 198)
+    a = process('test')
+    call assert(a(1), 198)
+    call assert(a(2), 230)
 
-    print *, "Part1:", part1('input')
+    a = process('input')
+    print *, "Part1:", a(1)
+    print *, "Part2:", a(2)
 contains
 
-function part1(filename)
+! Return true if the most common bit value is 1
+function common_bit(bits, pos)
+    character(*) :: bits(:)
+    integer :: pos
+    integer :: one_count
+    character :: common_bit
+
+    one_count = count(bits(:)(pos:pos) == '1')
+    if (one_count * 2 >= size(bits)) then
+        common_bit = '1'
+    else
+        common_bit = '0'
+    end if
+end function
+
+function process(filename) result(part)
     character(*) :: filename
-    integer :: part1
+    integer :: part(2)
     integer :: fd
     integer :: stat
-    character(32) :: bits
-    integer :: i
+    character(32) :: line
+    character(:), allocatable :: bits(:)
+    integer :: i, j
     integer :: nbits
-    integer, allocatable :: bitsum(:)
     integer :: nlines
-    integer :: g
-    integer :: e
+    integer :: g, e
+    integer :: top, bot
 
     open(newunit=fd, action='read', file=filename, iostat=stat)
-
-    read (fd, *, iostat=stat) bits
-    nbits = len_trim(bits)
     nlines = 0
-    allocate(bitsum(nbits))
-    bitsum = 0
     do
-        nlines = nlines + 1
-        bitsum = bitsum + (/ (iachar(bits(i:i)) - iachar('0'), i=1,nbits) /)
-        read (fd, *, iostat=stat) bits
+        read (fd, *, iostat=stat) line
         if (stat /= 0) then
             exit
         end if
+        nlines = nlines + 1
+    end do
+    nbits = len_trim(line)
+    rewind(fd)
+
+    allocate(character(nbits) :: bits(nlines))
+    do i = 1,nlines
+        read (fd, *, iostat=stat) bits(i)
     end do
     close(fd)
 
@@ -43,14 +64,61 @@ function part1(filename)
     do i=1,nbits
         g = g * 2
         e = e * 2
-        if (bitsum(i) > (nlines / 2)) then
-            g =g + 1
+        if (common_bit(bits, i) == '1') then
+            g = g + 1
         else
             e = e + 1
         end if
     end do
-    !print *, g, e
-    part1 = g * e
+
+
+    part(1) = g * e
+
+    call partition(bits, 1, nlines, 1)
+    part(2) = binstr(bits(1)) * binstr(bits(nlines))
 end function
+
+function binstr(s) result (val)
+    character(*) s
+    integer :: val
+
+    read (s, "(B32)") val
+end function
+
+recursive subroutine partition(bits, top, bot, pos)
+    character(:), allocatable :: bits(:)
+    integer, intent(in) :: top
+    integer, intent(in) :: bot
+    integer, intent(in) :: pos
+    character :: com
+    character(len(bits)) :: tmp
+    integer :: a, b
+
+    com = common_bit(bits(top:bot), pos)
+    a = top
+    b = bot
+    do
+        do while (a < bot .and. bits(a)(pos:pos) == com)
+            a = a + 1
+        end do
+        do while (b > top .and. bits(b)(pos:pos) /= com)
+            b = b - 1
+        end do
+        if (a >= b) then
+            exit
+        end if
+        tmp = bits(a)
+        bits(a) = bits(b)
+        bits(b) = tmp
+    end do
+    a = a - 1
+    b = b + 1
+    if (top == 1 .and. a > 1) then
+        call partition(bits, top, a, pos + 1)
+    end if
+    if (bot == size(bits) .and. b < size(bits)) then
+        call partition(bits, b, bot, pos + 1)
+    end if
+end subroutine
 
 end program
