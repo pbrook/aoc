5c0341d4e68b73b4e219d88d44f17cdb950cd93d
Paul Brook <paul@nowt.org>
2021-12-10 18:10:03 +0000

    2021/10 #2

--- 2021/10/main.f90
+++ 2021/10/main.f90
@@ -3,28 +3,32 @@ program main
 
     implicit none
 
-    integer :: a(2)
+    integer(8) :: a(2)
 
     a = parse('test')
-    call assert(a(1), 26397)
-    !call assert(a(2), 1134)
+    call assert8(a(1), 26397_8)
+    call assert8(a(2), 288957_8)
 
     a = parse('input')
     print *, "Part1:", a(1)
-    !print *, "Part2:", a(2)
+    print *, "Part2:", a(2)
 contains
 
 
 function parse(filename) result(part)
     character(*) :: filename
-    integer :: part(2)
+    integer(8) :: part(2)
     integer :: fd
     integer :: stat
 
     character(200) :: line
     character :: c
     integer :: i, pos
+    integer(8) :: complete(100)
+    integer :: ncomplete
+    integer(8) :: score
 
+    ncomplete = 0
     part(1) = 0
     open(newunit=fd, action='read', file=filename)
     do
@@ -54,8 +58,28 @@ function parse(filename) result(part)
         end do
         if (pos == -1) then
             part(1) = part(1) + part1(c)
+        else
+            score=0
+            do i=pos,1,-1
+                if (score * 5 < score) then
+                    error stop
+                end if
+                score = score * 5 + part2(line(i:i))
+            end do
+            if (ncomplete == size(complete)) then
+                error stop
+            end if
+            ncomplete = ncomplete + 1
+            complete(ncomplete) = huge(score)
+            do i=1,ncomplete
+                if (complete(i) > score) then
+                    complete(i:) = eoshift(complete(i:), -1, score)
+                    exit
+                end if
+            end do
         end if
     end do
+    part(2) = complete(ncomplete/2+1)
 end function
 
 function match(a, b)
@@ -89,6 +113,24 @@ function part1(c)
         part1 = 1197
     case ('>')
         part1 = 25137
+    case default
+        error stop
+    end select
+end function
+
+function part2(c)
+    character :: c
+    integer :: part2
+
+    select case (c)
+    case ('(')
+        part2 = 1
+    case ('[')
+        part2 = 2
+    case ('{')
+        part2 = 3
+    case ('<')
+        part2 = 4
     case default
         error stop
     end select
