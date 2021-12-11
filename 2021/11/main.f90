6844d8bb8a85e61a18ceea997257756754be35a8
Paul Brook <paul@nowt.org>
2021-12-11 18:16:19 +0000

    2021/11 #1

---
+++ 2021/11/main.f90
@@ -0,0 +1,87 @@
+program main
+    use aoc
+
+    implicit none
+
+    integer :: a(2)
+
+    a = octop('test')
+    call assert(a(1), 1656)
+    !call assert(a(2), )
+
+    a = octop('input')
+    print *, "Part1:", a(1)
+    !print *, "Part2:", a(2)
+contains
+
+
+function octop(filename) result(part)
+    character(*) :: filename
+    integer :: part(2)
+    integer :: fd
+    integer :: stat
+
+    integer :: grid(0:11,0:11)
+    integer :: i
+
+    grid=0 
+    open(newunit=fd, action='read', file=filename)
+    do i=1,10
+        read (fd, "(*(i1))") grid(1:10,i)
+    end do
+    close(fd)
+    part(1) = 0
+    do i=1,100
+        part(1) = part(1) + step(grid)
+        !print *, i
+        !call dump(grid)
+    end do
+end function
+
+subroutine dump(grid)
+    integer, intent(inout) :: grid(0:,0:)
+    integer :: i
+    do i=1,10
+        print "(*(i1))", grid(1:10,i)
+    end do
+end subroutine
+
+function step(grid) result(flash)
+    integer, intent(inout) :: grid(0:,0:)
+    integer :: flash
+    logical :: again
+    integer :: x, y
+
+    flash = 0
+    grid = grid + 1
+    again = .true.
+    do while (again)
+        again = .false.
+        do x=1,10
+            do y=1,10
+                if (grid(x,y) > 9) then
+                    grid(x,y) = -huge(grid)
+                    call inc(grid(x-1,y))
+                    call inc(grid(x+1,y))
+                    call inc(grid(x,y-1))
+                    call inc(grid(x,y+1))
+                    call inc(grid(x-1,y-1))
+                    call inc(grid(x+1,y-1))
+                    call inc(grid(x-1,y+1))
+                    call inc(grid(x+1,y+1))
+                    again = .true.
+                    flash = flash + 1
+                end if
+            end do
+        end do
+    end do
+    where (grid < 0)
+        grid = 0
+    end where
+end function
+
+subroutine inc(x)
+    integer, intent(inout) :: x
+    x = x + 1
+end subroutine
+end program
