325679602c652fdf9d05156532d7d60d47cc5a21
Paul Brook <paul@nowt.org>
2021-12-17 13:02:39 +0000

    2021/17 #2

--- 2021/17/main.f90
+++ 2021/17/main.f90
@@ -4,16 +4,17 @@ program main
     implicit none
 
     integer :: xmin, xmax, ymin, ymax
+    integer, parameter :: HIT = 0, MISS = 1, MISS_STOP = 2
 
     integer :: a(2)
 
     a = probe('test')
     call assert(a(1), 45)
-    !call assert(a(2), 1)
+    call assert(a(2), 112)
 
     a = probe('input')
     print *, "Part1:", a(1)
-    !print *, "Part2:", a(2)
+    print *, "Part2:", a(2)
 contains
 
 function probe(filename) result(part)
@@ -23,8 +24,7 @@ function probe(filename) result(part)
     integer :: stat
 
     character(12) :: xstr, ystr
-    integer :: y
-    integer :: height
+    integer :: x, y
 
     open(newunit=fd, action='read', file=filename)
     read(fd, "(a12)", advance='no') xstr
@@ -35,10 +35,48 @@ function probe(filename) result(part)
     call decode_range(xstr, xmin, xmax)
     call decode_range(ystr, ymin, ymax)
 
-    if (ymin >= 0) then
+    if (ymin >= 0 .or. xmin < 0) then
         error stop
     end if
     part(1) = ymin * (ymin+1) / 2
+    part(2) = 0
+    do x=0,xmax+1
+        do y=ymin,-ymin
+            if (fire(x, y)) then
+                part(2) = part(2) + 1
+            end if
+        end do
+    end do
+end function
+
+function fire(dx, dy)
+    integer, value :: dx, dy
+    logical :: fire
+    integer :: x, y
+
+    x = 0
+    y = 0
+    do
+        x = x + dx
+        y = y + dy
+        if (dx > 0) then
+            dx = dx - 1
+        end if
+        dy = dy - 1
+
+        if (dx == 0 .and. x < xmin) then
+            fire = .false.
+            exit
+        end if
+        if (x > xmax .or. y < ymin) then
+            fire = .false.
+            exit
+        end if
+        if (x >= xmin .and. y <= ymax) then
+            fire = .true.
+            exit
+        end if
+    end do
 end function
 
 subroutine decode_range(s, from, to)
