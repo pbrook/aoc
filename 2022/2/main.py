ebb5b7cfa2147e711a6dc04c94ea92f70a91e210
Paul Brook <paul@nowt.org>
2022-12-02 12:18:18 +0000

    2022/2 #2

--- 2022/2/main.py
+++ 2022/2/main.py
@@ -1,25 +1,36 @@
 #! /usr/bin/env python3
 
-def play(line):
+def part1(them, me):
+    return me
+
+def part2(them, me):
+    return (them + me + 2) % 3
+
+def play(line, part):
     them_c, me_c = line.split()
-    them = ord(them_c) + 1 - ord('A')
-    me = ord(me_c) + 1 - ord('X')
+    them = ord(them_c) - ord('A')
+    me = ord(me_c) - ord('X')
+
+    me = part(them, me)
 
     if them == me: # Draw
         points = 3
-    elif ((them + 1) % 3) == (me % 3): # Win
+    elif ((them + 1) % 3) == me: # Win
         points = 6
     else: # Loose
         points = 0
-    return points + me
+    return points + me + 1
 
-def rps(filename):
+def rps(filename, part):
     score = 0
     with open(filename, "r") as f:
         for line in f:
-            score += play(line)
+            score += play(line, part)
     return score
 
-assert rps("test1") == 15
+assert rps("test1", part1) == 15
+assert rps("test1", part2) == 12
 
-print(rps("input"))
+print(rps("input", part1))
+print(rps("input", part2))
+
