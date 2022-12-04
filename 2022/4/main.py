ac87858fc6cd163e91b4cb46724b671a80c5b620
Paul Brook <paul@nowt.org>
2022-12-04 11:26:11 +0000

    2022/4 #2

--- 2022/4/main.py
+++ 2022/4/main.py
@@ -7,19 +7,28 @@ def getrange(s):
 def contains(a, b):
     return a[0] <= b[0] and a[1]>= b[1]
 
-def part1(filename):
+
+def part1(a, b):
+    return contains(a, b) or contains(b, a)
+
+def part2(a, b):
+    return not (a[0] > b[1] or a[1] < b[0])
+
+def scan(filename, part):
     total = 0
     with open(filename, "r") as f:
         for line in f:
             astr, bstr = line.strip().split(",")
             a = getrange(astr)
             b = getrange(bstr)
-            if contains(a, b) or contains(b, a):
+            if part(a, b):
                 total += 1
     return total
 
-assert part1("test1") == 2
+assert scan("test1", part1) == 2
+assert scan("test1", part2) == 4
 
-print(part1("input"))
+print(scan("input", part1))
+print(scan("input", part2))
             
 
