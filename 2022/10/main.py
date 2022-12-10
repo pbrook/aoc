e53c4435d1f47f7fdc82bea1132bc0c6959943ca
Paul Brook <paul@nowt.org>
2022-12-10 13:17:46 +0000

    2022/10 #2

--- 2022/10/main.py
+++ 2022/10/main.py
@@ -1,26 +1,52 @@
 #! /usr/bin/env python3
 
-def signal(cycle, x):
-    if cycle not in [20, 60, 100, 140, 180, 220]:
-        return 0
-    return cycle * x
+class Cpu:
+    def __init__(self, filename):
+        self.signal = 0
+        self.cycle = 1
+        self.x = 1
+        self.screen = []
+        self.scanline = ""
+        with open(filename, "r") as f:
+            for line in f:
+                self.step()
+                insn = line.split()
+                if insn[0] == "addx":
+                    self.step()
+                    self.x += int(insn[1])
 
-def cpu(filename):
-    total = 0
-    cycle = 1
-    x = 1
-    with open(filename, "r") as f:
-        for line in f:
-            total += signal(cycle, x)
-            insn = line.split()
-            if insn[0] == "addx":
-                cycle += 1
-                total += signal(cycle, x)
-                x += int(insn[1])
-            cycle += 1
-    total += signal(cycle, x)
-    return total
+    def step(self):
+        if self.cycle in [20, 60, 100, 140, 180, 220]:
+            self.signal += self.cycle * self.x
+        n = len(self.scanline)
+        if abs(n - self.x) <= 1:
+            self.scanline += '#'
+        else:
+            self.scanline += '.'
+        if n == 39:
+            self.screen.append(self.scanline)
+            self.scanline = ""
+        self.cycle += 1
 
-assert cpu("test1") == 13140
+    def part1(self):
+        return self.signal
 
-print(cpu("input"))
+    def part2(self):
+        return self.screen
+
+s = [
+    "##..##..##..##..##..##..##..##..##..##..",
+    "###...###...###...###...###...###...###.",
+    "####....####....####....####....####....",
+    "#####.....#####.....#####.....#####.....",
+    "######......######......######......####",
+    "#######.......#######.......#######.....",
+    ]
+
+c = Cpu("test1")
+assert c.part1() == 13140
+assert c.part2() == s
+
+c = Cpu("input")
+print(c.part1())
+print("\n".join(c.part2()))
