6879f7d905542e016dc078f3b51a297d7a44ee7c
Paul Brook <paul@nowt.org>
2022-12-19 23:05:40 +0000

    2022/19 #1 bit quicker

--- 2022/19/main.py
+++ 2022/19/main.py
@@ -1,30 +1,35 @@
 #! /usr/bin/env python3
 
+N = 24
+
 class Blueprint:
     def __init__(self, line):
         self.cost = {}
         _, s = line.split(':')
+        self.max = {"geode":9999}
         for r in s.split('.'):
             ar = r.split()
             if not ar:
                 continue
             assert (ar[0], ar[2], ar[3]) == ('Each', 'robot', 'costs')
-            self.cost[ar[1]] = [(ar[i+1], int(ar[i])) for i in range(4, len(ar), 3)]
+            cost = [(ar[i+1], int(ar[i])) for i in range(4, len(ar), 3)]
+            self.cost[ar[1]] = cost
+            for rr, n in cost:
+                self.max[rr] = max(n, self.max.get(rr, 0))
     def __repr__(self):
         return f"<{self.cost}>"
     def tick(self, bots, res, time, ignore=None):
         if ignore is None:
             ignore = set()
-        if time > 23:
-            print(f"  {time}")
         best = 0
         nr = dict((r, n + res[r]) for r, n in bots.items())
         time -= 1
         if time == 0:
             return nr["geode"]
-        tried = False
         for name, cost in self.cost.items():
             if name in ignore:
+                continue
+            if bots[name] >= self.max[name]:
                 continue
             if any(res[r] < n for r, n in cost):
                 continue
@@ -34,12 +39,17 @@ class Blueprint:
                 nrb[r] -= n
             nb = bots.copy()
             nb[name] += 1
+            if time >= N:
+                print(f"  {time} {name}")
             count = self.tick(nb, nrb, time)
             if count > best:
                 best = count
-        count = self.tick(bots, nr, time, ignore)
-        if count > best:
-            best = count
+        if "geode" not in ignore:
+            if time >= N:
+                print(f"  {time} -")
+            count = self.tick(bots, nr, time, ignore)
+            if count > best:
+                best = count
         return best
 
 def parse(filename):
@@ -65,11 +75,11 @@ def part2(filename):
     bots = nothing.copy()
     bots["ore"] = 1
     prod = 1
-    for i, bp in enumerate(blueprints[:3]):
+    for i, bp in enumerate(blueprints[1:3]):
         count = bp.tick(bots=bots, res=nothing, time=32)
         print(i+1, count)
         prod *= count
     return prod
 
-#print(part1("test1"))
+print(part1("test1"))
 print(part1("input"))
