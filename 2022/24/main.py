cc0291cdcfcdff7e738bc1ff560a706c6293c3a1
Paul Brook <paul@nowt.org>
2022-12-24 12:49:16 -0500

    2022/24 #1

---
+++ 2022/24/main.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python3
+
+ortho = [(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)]
+face = {
+    '>': (1, 0),
+    '<': (-1, 0),
+    'v': (0, 1),
+    '^': (0, -1),
+}
+
+def dist(x, y, target):
+    return abs(x-target[0]) + abs(y-target[1])
+
+class Storm():
+    def __init__(self, filename):
+        ns = []
+        self.sdir = []
+        with open(filename, "r") as f:
+            s = f.read().rstrip().split('\n')
+            assert s[0][:3] == '#.#' and all(c == '#' for c in s[0][3:])
+            assert s[-1][-3:] == '#.#' and all(c == '#' for c in s[-1][:-3])
+            for y, line in enumerate(s[1:-1]):
+                line = line.strip()
+                assert line[0] == '#' and line[-1] == '#'
+                for x, c in enumerate(line[1:-1]):
+                    if c == '.':
+                        continue
+                    self.sdir.append(face[c])
+                    ns.append((x, y))
+        self.ar = [ns]
+        self.width = len(s[0]) - 2
+        self.height = len(s) - 2
+
+    def get(self, n):
+        while n >= len(self.ar):
+            ns = []
+            for (sx, sy), (dx, dy) in zip(self.ar[-1], self.sdir):
+                pos = ((sx + dx) % self.width, (sy + dy) % self.height)
+                ns.append(pos)
+            self.ar.append(ns)
+        return self.ar[n]
+
+    def walk(self, start, target, steps):
+        steps += 1
+        while start in self.get(steps):
+            steps += 1
+        done = set()
+        x, y = start
+        stack = [(dist(x, y, target) + steps, x, y, steps)]
+        while stack:
+            stack.sort(reverse=True)
+            _, x, y, steps = stack.pop()
+            #print(x, y, steps, _, len(stack))
+            steps += 1
+
+            newpos = set()
+            for dx, dy in ortho:
+                if (x + dx) in range(self.width) and (y+dy) in range(self.height):
+                    newpos.add((x+dx, y+dy))
+            if len(newpos) == 0:
+                continue
+            newpos.difference_update(self.get(steps))
+            for x, y in newpos:
+                limit = dist(x, y, target)
+                #print(f" {x} {y} {limit}")
+                if limit == 0:
+                    return steps + 1
+                else:
+                    if (x, y, steps) not in done:
+                        done.add((x, y, steps))
+                        stack.append((limit+steps, x, y, steps))
+        assert False
+
+def run(filename):
+    s = Storm(filename)
+    goal = (s.width - 1), (s.height - 1)
+    part1 = s.walk((0,0), goal, 0)
+    return part1
+
+assert run("test1") == 10
+assert run("test2") == 18
+
+print(run("input"))
