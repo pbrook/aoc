08724ae4de32ca5cc54a498f6da4b7c01a97a07c
Paul Brook <paul@nowt.org>
2022-12-28 12:53:46 -0500

    2015/4 #2

--- 2015/4/main.ml
+++ 2015/4/main.ml
@@ -1,14 +1,20 @@
+let input = "yzbqklnj"
+
 let goal = String.make 5 '0'
 
-let rec mine data n =
-    let md5 = Digest.string (data ^ Int.to_string n) |> Digest.to_hex in
-    if String.starts_with ~prefix:goal md5 then
-        n
-    else begin
-        mine data (n+1)
-    end
+let mine data len =
+    let goal = String.make len '0' in
+    let rec find n =
+        let md5 = Digest.string (data ^ Int.to_string n) |> Digest.to_hex in
+        if String.starts_with ~prefix:goal md5 then
+            n
+        else
+            find (n + 1)
+    in
+    find 1
 
 let () = 
-    assert (mine "abcdef" 1 == 609043);
-    assert (mine "pqrstuv" 1 == 1048970);
-    Printf.printf "%d\n" (mine "yzbqklnj" 1)
+    assert (mine "abcdef" 5 == 609043);
+    assert (mine "pqrstuv" 5 == 1048970);
+    Printf.printf "%d\n%!" (mine input 5);
+    Printf.printf "%d\n" (mine input 6)
