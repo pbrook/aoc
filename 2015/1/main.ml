e781ff6b8f160e127b2c4dcf5cdff3b43d7018fc
Paul Brook <paul@nowt.org>
2022-12-28 10:56:39 -0500

    2015/1 #2

--- 2015/1/main.ml
+++ 2015/1/main.ml
@@ -1,14 +1,28 @@
-let rec parse ic = 
-        try 
-            let n = match input_char ic with
-            | '(' -> 1
-            | ')' -> -1
-            | _ -> assert false
-            in n + parse ic
-        with End_of_file -> 0
+let parse c = match c with
+    | '(' -> 1
+    | ')' -> -1
+    | _ -> assert false
+
+let rec readfile ic =
+    try
+        let c = input_char ic in
+        parse c :: readfile ic
+    with End_of_file -> []
 
 let part1 filename =
-    let ic = open_in filename in
-    parse ic
+    let inp = readfile (open_in filename) in
+    List.fold_left (+) 0 inp
 
-let () = print_int (part1 "input") ; print_newline ()
+let rec find_basement pos s =
+    match s with
+    | x::xs -> let np = pos + x in
+        if np == -1 then 1 else 1 + find_basement np xs
+    | [] -> assert false
+
+let part2 filename =
+    let inp = readfile (open_in filename) in
+    find_basement 0 inp
+
+let () = 
+    Printf.printf "%d\n" (part1 "input");
+    Printf.printf "%d\n" (part2 "input")
