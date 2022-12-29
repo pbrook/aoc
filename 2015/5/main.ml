e0ee874db19c62d9a7f60dcf4d6b2fccf7d504c0
Paul Brook <paul@nowt.org>
2022-12-28 20:47:38 -0500

    2015/5 #2

--- 2015/5/main.ml
+++ 2015/5/main.ml
@@ -3,14 +3,16 @@ let vowels = "aeiou"
 let vowel_count n c =
     n + if String.contains vowels c then 1 else 0
 
-let rec isdup s n =
-    if n > String.length s - 2 then
-        false
-    else
-        if s.[n] == s.[n+1] then
+let trystring ?(start=0) f s len =
+    let rec loop n =
+        if n > String.length s - len then
+            false
+        else if f s n then
             true
         else
-            isdup s (n+1)
+            loop (n+1)
+    in
+    loop start
 
 let badstr = Str.regexp {|ab\|cd\|pq\|xy|}
 let hasbad s =
@@ -19,22 +21,31 @@ let hasbad s =
         true
     with Not_found -> false
 
-let is_nice s =
+let part1 s =
     let nv = String.fold_left vowel_count 0 s in
-    let dup = isdup s 0 in
+    let dup = trystring (fun s n -> s.[n] == s.[n+1]) s 2 in
     nv >= 3 && dup && not (hasbad s)
 
+let ispair s n0 =
+    let a, b = s.[n0], s.[n0+1] in
+    trystring ~start:(n0+2) (fun s n -> s.[n] == a && s.[n+1] == b) s 2 
 
-let part1 filename =
+let part2 s =
+    let pair = trystring ispair s 4 in
+    let aba = trystring (fun s n -> s.[n] == s.[n+2]) s 3 in
+    pair && aba
+
+let test part filename =
     let count = ref 0 in
     let ic = open_in filename in
     try
         while true do
             let line = input_line ic in
-            if is_nice line then incr count
+            if part line then incr count
         done;
         assert false
     with End_of_file -> !count
 
 let () = 
-    Printf.printf "%d\n" (part1 "input")
+    Printf.printf "%d\n" (test part1 "input");
+    Printf.printf "%d\n" (test part2 "input")
